c115760ec32c246c3d7504a0180aa3954d3435d8
Marco Ferragina <marco.ferragina@gmail.com>
2023-05-21 21:38:41 +0200

    handle async req

--- main.go
+++ main.go
@@ -7,8 +7,10 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/charmbracelet/bubbles/spinner"
 	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
 
 func main() {
@@ -18,24 +20,35 @@ func main() {
 	}
 }
 
+type aiResponsesMsg struct {
+	Content string
+}
+
 type (
 	errMsg error
 )
 
 type model struct {
 	textInput textinput.Model
+	spinner   spinner.Model
 	ai        *AI
 	err       error
 }
 
 func initialModel() model {
+
 	ti := textinput.New()
 	ti.Placeholder = "Ask"
 	ti.Focus()
 	ti.CharLimit = 1000
 	ti.Width = 60
 
+	s := spinner.New()
+	s.Spinner = spinner.Dot
+	s.Style = lipgloss.NewStyle().Foreground(lipgloss.Color("205"))
+
 	return model{
+		spinner:   s,
 		textInput: ti,
 		ai:        newAI(),
 		err:       nil,
@@ -43,26 +56,37 @@ func initialModel() model {
 }
 
 func (m model) Init() tea.Cmd {
-	return textinput.Blink
+	return tea.Batch(textinput.Blink, m.spinner.Tick)
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmds []tea.Cmd
 	var cmd tea.Cmd
 
 	switch msg := msg.(type) {
+	case aiResponsesMsg:
+		cmds = append(cmds, tea.Printf("AI:\t%s", msg.Content))
+
 	case tea.KeyMsg:
 		switch msg.Type {
 		case tea.KeyCtrlC, tea.KeyEsc:
 			return m, tea.Quit
+
 		case tea.KeyEnter:
 			value := m.textInput.Value()
 			m.textInput.Reset()
-			res, err := m.ai.ask(value)
-			if err != nil {
-				m.err = err
-				return m, nil
+			cmds = append(cmds, tea.Printf("You:\t%s", value))
+
+			cmd = func() tea.Msg {
+				res, err := m.ai.ask(value)
+				if err != nil {
+					return err
+				}
+				return aiResponsesMsg{
+					Content: res,
+				}
 			}
-			return m, tea.Printf("You:\t%s\nAI:\t%s", value, res)
+			cmds = append(cmds, cmd)
 		}
 
 	// We handle errors just like any other message
@@ -72,7 +96,12 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	}
 
 	m.textInput, cmd = m.textInput.Update(msg)
-	return m, cmd
+	cmds = append(cmds, cmd)
+
+	m.spinner, cmd = m.spinner.Update(msg)
+	cmds = append(cmds, cmd)
+
+	return m, tea.Batch(cmds...)
 }
 
 func (m model) View() string {
